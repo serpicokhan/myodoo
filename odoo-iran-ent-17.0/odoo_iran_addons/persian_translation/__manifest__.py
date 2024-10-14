1d22d34adeb29b9d1f7bc41aa81de1aab459f8e8
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2024-10-14 09:53:56 +0330

    init

---
+++ odoo-iran-ent-17.0/odoo_iran_addons/persian_translation/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024-Today: Odoo Community Iran
+# @author: Odoo Community Iran (https://odoo-community.ir/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Persian Translator",
+    "summary": """Persian Translator""",
+    "author": "Odoo Community Iran",
+    "license": "AGPL-3",
+    "website": "https://odoo-community.ir/",
+    "category": "Technical",
+    "version": "1.0.2",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/base_import_language_views.xml",
+    ],
+}
